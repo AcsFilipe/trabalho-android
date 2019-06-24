d44abe319dda1388c1fa27a4d5038e4faf9e0f8c
FilipeAraujo <acsfilipe@gmail.com>
2019-06-24 20:37:11 -0300

    atualizando

--- app/src/main/java/com/example/trabalho1/MainActivity.java
+++ app/src/main/java/com/example/trabalho1/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.trabalho1;
 
 import android.Manifest;
-import android.content.Context;
-import android.content.ContextWrapper;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -17,18 +14,14 @@ import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 
 
